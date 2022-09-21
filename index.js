439bac16851416a04924c5cf05e0496f32e18ab9
Dulat <dulat.kussainov@gmail.com>
2022-09-21 12:42:36 -0400

    commit tree 776c974b6289d2c845e7068ecbe5e68c4b631e0b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+const cardNames = ["Guadalupe", "Ollie", "Aki"];
+const cardEvent = 'surprise'
+let thankYouCards = [];
+function writeCards(cardNames, cardEvent) {
+    
+    for (i = 0; i < cardNames.length; i++) {
+        thankYouCards.push(`Thank you, ${cardNames[i]}, for the wonderful ${cardEvent} gift!`)
+    }
+    return thankYouCards;
+}
+
+console.log(writeCards(cardNames, cardEvent));
+
+
